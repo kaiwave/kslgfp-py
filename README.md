bffec638f09badeda2dd04b8f4e55b697cf6e594
kaiwave <malakaiharianto@gmail.com>
2026-08-25 19:12:45 +1000

    patch 0.3 - sentences and second shots

--- README.md
+++ README.md
@@ -8,13 +8,19 @@ may add more features later but nothing specific is planned...
 
 updates below mostly so i can keep track of what im doing:
 
-- patch 0.2
+- **patch 0.3**
+- - added sentence mode, a way to practice translating full sentences, with hints available (shuffled answers). eg. ("i think therefore i am" with hints adds a line `Hint: sum ergo cogito`)
+- - done in program, no need to put a dedicated hint inside ur own .csv file
+- - added (optional) re-attempts for missed cards (until u get 100%), so u can practice ur weaknesses a bit better
+
+
+- **patch 0.2**
 - - colors !! green for correct answers, red for incorrect. final % is colored red for <50%, yellow for 50%-85% and green for >85%
 - - databases moved to `databases/`, and loading is now easier, only type the name of file. (`databases/` prefix and `.csv` extension not mandatory to enter anymore)
 - - refactored basically everything out of main function to help w future expansion development and stuff
 
 
-- patch 0.1
+- **patch 0.1**
 - - added inline hint support, supposed to be for conjugation endings but can be for anything (eg. "amare" asks "Your answer: am", and u only need to put the ending)
 - - add some `verb root-` before actual prompt (dash is important!!) 
 - - see notation example in [default.csv](databases/default.csv) - (eg: "`am-amare 1st person singular present tense"`)
--- main.py
+++ main.py
@@ -21,8 +21,40 @@ def load_database(filepath):  #database loading prog
         print(f"Could not find '{filepath}'.")  # basic failsafe
         sys.exit(1)
 
+def grade_user(correct,total,missed_cards):
+    if correct == total:
+        print(f"\nSession Complete! Score: {correct}/{total} (\033[32m100.0%\033[0m)")
+        print(f"\nCongrats, you nailed it!")
+        return
+    elif 0.85 <= correct/total < 1:
+        print(f"\nSession Complete! Score: {correct}/{total} (\033[32m{(correct / total * 100):.1f}%\033[0m)")  # score
+    elif 0.50 <= correct/total < 0.85:
+        print(f"\nSession Complete! Score: {correct}/{total} (\033[33m{(correct / total * 100):.1f}%\033[0m)")
+    else:
+        print(f"\nSession Complete! Score: {correct}/{total} (\033[31m{(correct / total * 100) if total else 0:.1f}%\033[0m)")
 
-def run_quiz(cards):
+    try_again = input(f"\nYou missed {total - correct} cards! Would you like to try them again? (y/N) ")
+
+    if try_again.lower() == "y":
+        selected_type = "missed"
+        run_quiz(missed_cards, selected_type)
+    else:
+        return
+
+def scramble_hints(answer): # scramble words for hints for sentences
+    words = answer.split()
+
+    if len(words) > 1:
+        shuffled = words.copy()
+        while shuffled == words:
+            random.shuffle(shuffled)
+        words = shuffled
+    else:
+        random.shuffle(words)
+
+    return " ".join(words).lower()
+
+def run_quiz(cards, selected_type):
     if not cards:
         print("No cards found for this choice")  # check if cards of type selected are available
         return
@@ -30,17 +62,26 @@ def run_quiz(cards):
     random.shuffle(cards)
     correct = 0
     total = len(cards)  # setup var
+    hints_flag = False
+    missed_cards = []
+
+    if selected_type == "sentence":
+        hints = input(f"\nStarting sentence session with {total} cards. Would you like hints? (y/N) : \n")  # warns user how many cards to do
+
+        if hints.lower() == 'y':
+            hints_flag = True
 
     print(f"\nStarting session with {total} cards. Type 'q' to exit early.\n")  # warns user how many cards to do
 
     for i, card in enumerate(cards, 1):  # actual game loop
         try:
-            print(
-                f"[{i}/{total}] [{card['type'].upper()}] {card['prompt'].split('-', 1)[1]}")  # for new conjugation endings feature/inline prompt hints
+            print(f"[{i}/{total}] [{card['type'].upper()}] {card['prompt'].split('-', 1)[1]}")  # for new conjugation endings feature/inline prompt hints
             answer_prompt = card['prompt'].split('-', 1)[0]
             user_input = input(f"Your Answer: {answer_prompt}").strip()  # get answer
         except IndexError:
             print(f"[{i}/{total}] [{card['type'].upper()}] {card['prompt']}")  # fallback for if no inline prompt in csv
+            if hints_flag:
+                print(f"Hint: {scramble_hints(card['answer'])}") # prints hint if sentence mode
             user_input = input(f"Your Answer: ").strip()  # get answer
 
         if user_input.lower() == 'q':
@@ -52,18 +93,18 @@ def run_quiz(cards):
             correct += 1  #dopamine hit. score system
         else:
             print(f"\033[31mIncorrect.\033[0m Answer: {card['answer']}")  # correction
+            missed_cards.append({
+                "type": card["type"].strip().lower(),
+                "prompt": card["prompt"].strip(),
+                "answer": card["answer"].strip(),
+                "notes": card.get("notes", "").strip()  # load csv file into categories
+            })
 
         if card["notes"]:
             print(f"  Note: {card['notes']}")
         print("-" * 35)  # next line reset
 
-    if correct/total >= 0.85:
-        print(f"\nSession Complete! Score: {correct}/{total} (\033[32m{(correct / total * 100):.1f}%\033[0m)")  # score
-    elif 0.50 <= correct/total < 0.85:
-        print(f"\nSession Complete! Score: {correct}/{total} (\033[33m{(correct / total * 100):.1f}%\033[0m)")
-    else:
-        print(f"\nSession Complete! Score: {correct}/{total} (\033[31m{(correct / total * 100) if total else 0:.1f}%\033[0m)")
-
+    grade_user(correct, total, missed_cards)
 
 def choose_database():
     folder_prefix = Path("databases")
@@ -78,6 +119,8 @@ def choose_database():
     return database_choice
 
 def choose_type(card_types, all_cards):
+    selected_type = "all" # default selected type
+
     print("=== Language Flashcard Trainer ===")  # in principle can be any language
     print("1. All Cards")
     for idx, ctype in enumerate(card_types, 2):
@@ -94,16 +137,16 @@ def choose_type(card_types, all_cards):
         print("Invalid selection. Loading all cards.")
         selected_cards = all_cards  # failsafe for idiot users
 
-    return selected_cards
+    return selected_cards, selected_type
 
 def main():
     database_choice = choose_database()
     all_cards = load_database(database_choice)  # let user pick database
     card_types = sorted(list(set(c["type"] for c in all_cards)))  # sets up user choice for which card set to do
 
-    selected_cards = choose_type(card_types, all_cards)
+    selected_cards, selected_type = choose_type(card_types, all_cards)
 
-    run_quiz(selected_cards)  # run quiz
+    run_quiz(selected_cards, selected_type)  #run quiz normally
 
 if __name__ == "__main__":
     main()
